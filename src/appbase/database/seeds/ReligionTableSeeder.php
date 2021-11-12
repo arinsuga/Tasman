23f830d2b4c9f90b260cd93c0e0880ee8a5228be
unknown <arinsuga@gmail.com>
2021-11-12 13:42:44 +0700

    main - database lookup database table seed

--- src/appbase/database/seeds/ReligionTableSeeder.php
+++ src/appbase/database/seeds/ReligionTableSeeder.php
@@ -11,6 +11,16 @@ class ReligionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Reset table
+        DB::table("religion")->delete();
+
+        DB::table("religion")->insert([ "code" => "ISL", "name" => "Islam", "displayname" => "Islam", "description" => "Islam", "numsort" => 10, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "KRI", "name" => "Kristen", "displayname" => "Kristen", "description" => "Kristen", "numsort" => 20, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "KAT", "name" => "Katolik", "displayname" => "Katolik", "description" => "Katolik", "numsort" => 30, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "HIN", "name" => "Hindu", "displayname" => "Hindu", "description" => "Hindu", "numsort" => 40, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "BUD", "name" => "Buddha", "displayname" => "Buddha", "description" => "Buddha", "numsort" => 50, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "KHO", "name" => "Khonghucu", "displayname" => "Khonghucu", "description" => "Khonghucu", "numsort" => 60, "status" => 1, ]); 
+        DB::table("religion")->insert([ "code" => "SHI", "name" => "Shinto", "displayname" => "Shinto", "description" => "Shinto", "numsort" => 70, "status" => 1, ]); 
+        
     }
 }
