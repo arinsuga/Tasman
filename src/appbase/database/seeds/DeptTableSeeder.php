4fb51ebe9efc3e417a8d4785c699f2ebff231f50
unknown <arinsuga@gmail.com>
2021-11-17 16:03:21 +0700

    main - dept,subdept,bizunit

--- src/appbase/database/seeds/DeptTableSeeder.php
+++ src/appbase/database/seeds/DeptTableSeeder.php
@@ -13,14 +13,14 @@ class DeptTableSeeder extends Seeder
     {
         //Table reset
         DB::table("dept")->delete();
-        DB::table("dept")->insert([ "code" => "ART", "name" => "Artwork", "displayname" => "Komisaris", "description" => "Komisaris", "numsort" => 10, "status" => 1, ]); 
-        DB::table("dept")->insert([ "code" => "DIR", "name" => "Direksi", "displayname" => "Direksi", "description" => "Direksi", "numsort" => 20, "status" => 1, ]); 
+        DB::table("dept")->insert([ "code" => "KOM", "name" => "Komisaris", "displayname" => "Komisaris", "description" => "Komisaris", "numsort" => 10, "status" => 1, ]); 
+        DB::table("dept")->insert([ "code" => "BOD", "name" => "Board Of Director", "displayname" => "Direksi", "description" => "Direksi", "numsort" => 20, "status" => 1, ]); 
         DB::table("dept")->insert([ "code" => "ACCENT", "name" => "Accent", "displayname" => "Accent", "description" => "Accent", "numsort" => 30, "status" => 1, ]); 
         DB::table("dept")->insert([ "code" => "BALI", "name" => "Bali", "displayname" => "Bali", "description" => "Bali", "numsort" => 40, "status" => 1, ]); 
-        DB::table("dept")->insert([ "code" => "ACCENT", "name" => "Accent", "displayname" => "Graha Cipta Hadiprana", "description" => "Graha Cipta Hadiprana", "numsort" => 50, "status" => 1, ]); 
+        DB::table("dept")->insert([ "code" => "GCH", "name" => "Graha Cipta Hadiprana", "displayname" => "Graha Cipta Hadiprana", "description" => "Graha Cipta Hadiprana", "numsort" => 50, "status" => 1, ]); 
         DB::table("dept")->insert([ "code" => "ART", "name" => "Artwork", "displayname" => "Artwork", "description" => "Artwork", "numsort" => 60, "status" => 1, ]); 
-        DB::table("dept")->insert([ "code" => "KOM", "name" => "Komisaris", "displayname" => "Perencanaan", "description" => "Perencanaan", "numsort" => 70, "status" => 1, ]); 
-        DB::table("dept")->insert([ "code" => "DIR", "name" => "Direksi", "displayname" => "Support", "description" => "Support", "numsort" => 80, "status" => 1, ]); 
+        DB::table("dept")->insert([ "code" => "PRC", "name" => "Perencanaan", "displayname" => "Perencanaan", "description" => "Perencanaan", "numsort" => 70, "status" => 1, ]); 
+        DB::table("dept")->insert([ "code" => "SUP", "name" => "Support", "displayname" => "Support", "description" => "Support", "numsort" => 80, "status" => 1, ]); 
         
     }
 }
