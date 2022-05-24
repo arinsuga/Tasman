607eadd7b187b3a3dc7750f9bd6feaf58bbbeb40
unknown <arinsuga@gmail.com>
2022-05-24 11:30:37 +0700

    v1.0 - penyesuaian kolom report project: done next activity project : sesuaikan dropdown project sesuai dengan jenis project

--- src/appbase/resources/views/bo/project/report-detail-list-filters.blade.php
+++ src/appbase/resources/views/bo/project/report-detail-list-filters.blade.php
@@ -5,56 +5,46 @@
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter0" name="filter0" class="form-control float-right" placeholder="Requester" style="margin-left: 5px;">
+            <input type="text" id="filter0" name="filter0" class="form-control float-right" placeholder="Jenis Project" style="margin-left: 5px;">
             </div>
         </div>
     </div>
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter1" name="filter1" class="form-control float-right" placeholder="Department" style="margin-left: 5px;">
+            <input type="text" id="filter1" name="filter1" class="form-control float-right" placeholder="Project" style="margin-left: 5px;">
             </div>
         </div>
     </div>
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter2" name="filter2" class="form-control float-right" placeholder="Category" style="margin-left: 5px;">
+            <input type="text" id="filter2" name="filter2" class="form-control float-right" placeholder="Subject" style="margin-left: 5px;">
             </div>
         </div>
     </div>
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter3" name="filter3" class="form-control float-right" placeholder="Subcategory" style="margin-left: 5px;">
-            </div>
-        </div>
-    </div>
-    <div class="mt-2 row">
-        <div class="col-sm-12">
-            <div class="form-group">
-            <input type="text" id="filter4" name="filter4" class="form-control float-right" placeholder="Subject" style="margin-left: 5px;">
+            <input type="text" id="filter3" name="filter3" class="form-control float-right" placeholder="Description" style="margin-left: 5px;">
             </div>
         </div>
     </div>
 
+    <input type="hidden" id="filter4" name="4">
     <input type="hidden" id="filter5" name="5">
-    <input type="hidden" id="filter6" name="6">
-    <input type="hidden" id="filter7" name="7">
-    <input type="hidden" id="filter8" name="8">
-    <input type="hidden" id="filter9" name="9">
 
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter10" name="filter10" class="form-control float-right" placeholder="Start Time" style="margin-left: 5px;">
+            <input type="text" id="filter6" name="filter6" class="form-control float-right" placeholder="Start Time" style="margin-left: 5px;">
             </div>
         </div>
     </div>
     <div class="mt-2 row">
         <div class="col-sm-12">
             <div class="form-group">
-            <input type="text" id="filter11" name="filter11" class="form-control float-right" placeholder="Completed Time" style="margin-left: 5px;">
+            <input type="text" id="filter7" name="filter7" class="form-control float-right" placeholder="Completed Time" style="margin-left: 5px;">
             </div>
         </div>
     </div>
