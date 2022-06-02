1765d3ab08dd5dc304e3b01512ee8bfead40e378
unknown <arinsuga@gmail.com>
2022-06-02 09:59:15 +0700

    v1.0 - select query temp

---
+++ update_activity.sql
@@ -0,0 +1,49 @@
+use dbtasksheet;
+
+select *
+from activity;
+
+select *
+from employee;
+
+select *
+from dept;
+
+select a.id, a.enduser_id, b.name,
+b.dept_id, c.name as enduser_dept_name,
+b.subdept_id, d.name as enduser_subdept_name
+from activity a
+left join employee b
+on a.enduser_id = b.id
+left join dept c
+on b.dept_id = c.id
+left join subdept d
+on b.subdept_id = d.id;
+
+
+select a.*
+from activity a;
+
+select *
+from subdept;
+
+update activity a, 
+(
+	select a.id, a.enduser_id, b.name,
+	b.dept_id, c.name as enduser_dept_name,
+	b.subdept_id, d.name as enduser_subdept_name
+	from activity a
+	left join employee b
+	on a.enduser_id = b.id
+	left join dept c
+	on b.dept_id = c.id
+	left join subdept d
+	on b.subdept_id = d.id
+) as b
+set a.enduserdept_id = b.dept_id,
+    a.endusersubdept_id = b.subdept_id
+where a.id = b.id;
+
+
+
+
